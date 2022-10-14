6a7aaad76052ac4b1e6479e0991fe90acfd6befb
Lukas <urbanlukas@outlook.com>
2022-10-14 08:53:47 +0200

    Improve test

--- Migrator.Tests/ConnectionStringHelperTests.cs
+++ Migrator.Tests/ConnectionStringHelperTests.cs
@@ -32,7 +32,7 @@ namespace Migrator.Tests
         [InlineData("User ID=sa;Initial Catalog = DatabaseName; Data Source = localhost", "DatabaseName")]
         [InlineData("User ID=sa;Initial Catalog  = DatabaseName; Data Source = localhost", "DatabaseName")]
         [InlineData("User ID=sa;iNiTiAl CaTaLoG  = DatabaseName; Data Source = localhost", "DatabaseName")]
-        public void GetDatabaseFromConnectionString(string connectionString, string databaseName)
+        public void GetDatabase(string connectionString, string databaseName)
         {
             // Act
             var output = ConnectionStringHelper.GetDatabase(connectionString);
@@ -59,12 +59,23 @@ namespace Migrator.Tests
         [InlineData("Database = DatabaseName;Server = localhost;", "localhost")]
         [InlineData("Database = DatabaseName; Server = localhost;", "localhost")]
         [InlineData("Database = DatabaseName; sErVer = localhost;", "localhost")]
-        public void GetDataSourceFromConnectionString(string connectionString, string dataSource)
+        public void GetDataSource(string connectionString, string dataSource)
         {
             // Act
             var output = ConnectionStringHelper.GetDataSource(connectionString);
             // Assert
             Assert.Equal(dataSource, output);
+        }
+
+        [Theory]
+        [InlineData("Data Source = localhost; Database = DatabaseName;", "localhost")]
+        [InlineData("Data Source = localhost,123; Database = DatabaseName;", "localhost")]
+        public void GetLinkedServer(string connectionString, string linkedServer)
+        {
+            // Act
+            var output = ConnectionStringHelper.GetLinkedServer(connectionString);
+            // Assert
+            Assert.Equal(linkedServer, output);
         }
     }
 }
